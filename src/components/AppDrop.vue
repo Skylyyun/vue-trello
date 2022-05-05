0a868683c6971a95c4b57cc154b3b9d94d1ee17f
lyy <im.yiyun.lei@gmail.com>
2022-05-05 23:11:04 +0800

    Extracting Drag & Drop

---
+++ src/components/AppDrop.vue
@@ -0,0 +1,26 @@
+<template>
+<!-- not drop.self but drop.stop -->
+  <div
+    @drop.stop="onDrop"
+    @dragover.prevent
+    @dragenter.prevent
+  >
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onDrop(e) {
+      const transferData = JSON.parse(e.dataTransfer.getData('payload'))
+      // console.log(transferData)
+      this.$emit('drop', transferData)
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  
+</style>
--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -1,43 +1,50 @@
 <template>
-  <div
-    class="column"
-    draggable
-    @dragstart.self="pickUpColumn($event, columnIndex)"
-    @dragover.prevent
-    @dragenter.prevent
-    @drop="moveTaskOrColumn($event, column.tasks, columnIndex)"
+  <AppDrop
+    @drop="moveTaskOrColumn"
   >
-    <div class="flex items-center mb-2 font-bold">
-      {{ column.name }}
-    </div>
-    <div class="list-reset">
-      <ColumnTask
-        v-for='(task, $taskIndex) of column.tasks'
-        :key="$taskIndex"
-        :task="task"
-        :column="column"
-        :taskIndex="$taskIndex"
-        :columnIndex="columnIndex"
-        :board="board"
+    <AppDrag
+      class="column"
+      :transferData = "{
+        type: 'column',
+        fromColumnIndex: columnIndex
+      }"
+    >
+      <div class="flex items-center mb-2 font-bold">
+        {{ column.name }}
+      </div>
+      <div class="list-reset">
+        <ColumnTask
+          v-for='(task, $taskIndex) of column.tasks'
+          :key="$taskIndex"
+          :task="task"
+          :column="column"
+          :taskIndex="$taskIndex"
+          :columnIndex="columnIndex"
+          :board="board"
+        />
+      </div>
+      <input
+        type="text"
+        class="block p-2 w-full bg-transparent"
+        placeholder="+ Enter new task"
+        @keyup.enter="createTask($event, column.tasks)"
       />
-    </div>
-    <input
-      type="text"
-      class="block p-2 w-full bg-transparent"
-      placeholder="+ Enter new task"
-      @keyup.enter="createTask($event, column.tasks)"
-    />
-  </div>
+    </AppDrag>
+  </AppDrop>
 </template>
 
 <script>
 import ColumnTask from "./ColumnTask.vue"
+import AppDrop from "./AppDrop.vue"
+import AppDrag from "./AppDrag.vue"
 import movingTasksAndColumnsMixin from "../mixins/movingTasksAndColumnsMixin"
 
 export default {
   components: {
-    ColumnTask
-  },
+    ColumnTask,
+    AppDrag,
+    AppDrop
+},
   mixins: [movingTasksAndColumnsMixin],
   methods: {
     createTask (e, tasks) {
@@ -47,14 +54,7 @@ export default {
       })
       // clean the last input
       e.target.value = ''
-    },
-    pickUpColumn (e, fromColumnIndex) {
-      e.dataTransfer.effectAllowed = 'move'
-      e.dataTransfer.dropEffect = 'move'
-
-      e.dataTransfer.setData('from-column-index', fromColumnIndex)
-      e.dataTransfer.setData('type', 'column')
-    },
+    }
   }
 }
 </script>
